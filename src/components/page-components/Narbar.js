2b10a5be2df4a1846f9b64ad8b29d6e424e43de1
codecamp-official-thailand <codecamp.official.thailand@gmail.com>
2020-04-11 17:58:23 +0700

    all pages is created

---
+++ src/components/page-components/Narbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Narbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Narbar
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -8,24 +8,43 @@ const formItemLayout = {
 };
 
 function Login() {
+  function onFinish(values) {
+    console.log(values);
+  }
+
   return (
     <Row style={{ height: "100vh" }} justify="center" align="middle">
-      <Col>
+      <Col xs={15} sm={7} md={9} lg={7} xl={6} xxl={5}>
         <img
-          style={{ width: "386px" }}
+          style={{ width: "100%" }}
           alt="logo fakebook"
           src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
         />
       </Col>
-      <Col>
-        <Form {...formItemLayout}>
+      <Col xs={0} sm={2} md={2} lg={2} xl={2} xxl={2}></Col>
+      <Col xs={20} sm={9} md={9} lg={8} xl={6} xxl={5}>
+        <Form onFinish={onFinish} {...formItemLayout}>
           <Row>
-            <Form.Item label="Username" name="username">
+            <Form.Item
+              style={{ width: "100%" }}
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "กรุณากรอก Username ด้วยนะจ๊ะ" },
+              ]}
+            >
               <Input />
             </Form.Item>
           </Row>
           <Row>
-            <Form.Item label="Password" name="password">
+            <Form.Item
+              style={{ width: "100%" }}
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "กรุณากรอก Password ด้วยแหนะ" },
+              ]}
+            >
               <Input.Password />
             </Form.Item>
           </Row>
